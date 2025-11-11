07b91687a9f314dd8da70e919902ab0387f86fb9
Harold Guerrero <harold.guerrero02@uptc.edu.co>
2025-11-11 09:30:53 -0500

    inicio

---
+++ backend/aula-service/src/main/java/com/dtic/aula_service/AulaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dtic.aula_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaServiceApplication.class, args);
+	}
+
+}
